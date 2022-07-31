31309b472f5960a2fbcacda2b27e519be289e881
Renato Brito <dev.renatobrito@gmail.com>
2022-07-31 20:38:12 -0300

    Complete Style

--- assets/script/index.js
+++ assets/script/index.js
@@ -1,6 +1,5 @@
 const action = document.getElementById('action')
 const link_gerado = document.getElementById('link_gerado')
-const username = document.getElementById('username')
 const phoneNumber = document.getElementById('phoneNumber')
 const message = document.getElementById('message')
 const url = 'https://wa.me/'
@@ -8,5 +7,5 @@ const url = 'https://wa.me/'
 action.addEventListener('click', changeText)
 
 function changeText() {
-  link_gerado.textContent = `Username: @${username.value}  ${url}${phoneNumber.value}?text=${message.value}`
+  link_gerado.textContent = `${url}55${phoneNumber.value}?text=${message.value}`
 }
